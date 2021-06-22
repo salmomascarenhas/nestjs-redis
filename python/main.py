1ab6535284a198c92c19f9ba66ea8be286155837
Leonardo Gasparotto Menini <thelekerman@gmail.com>
2021-06-22 00:39:44 -0300

    Consertado o bug do node_modules | agora funcionar pegar por id os hash da fila

--- python/main.py
+++ python/main.py
@@ -1,11 +1,23 @@
+from typing import Dict
 from time import sleep
+import json
 import redis
 
 fila = redis.Redis(host="cache", port=6379, db=0)
 
+def get_hash(hash_id: int) -> None:
+
+    # Parsear campo que é objeto
+    data = fila.hget(f'bull:queue:{hash_id}', 'data')
+    print(json.loads(data)['data'])
+
+    # Parsear campo que é string
+    name = fila.hget(f'bull:queue:{hash_id}', 'name')
+    print(name.decode('utf8'))
+
 
 if __name__ == '__main__':
     print("iniciando servico")
+    get_hash(1)
     while True:
-        print('salve salve, ta no loop maluco')
         sleep(5)
